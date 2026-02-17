0d196d88ab3ab4dad04918f269e89fa7fb4b84b4
Nickson K Rop <nickyrutto@gmail.com>
2026-02-17 05:56:03 +0300

    simple addition in c++

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+	int m, n, sum;
+	std::cout << "Enter first number: ";
+	std::cin >> m;
+	std::cout << "Enter the second number: ";
+	std::cin >> n;
+	sum = m + n;
+	std::cout << sum;
+    return 0;
+}
