5822005d610dc698a7ab1a5bf295778bf3289cc0
Nickson K Rop <nickyrutto@gmail.com>
2026-04-28 19:29:36 +0300

    Disecting parts of a simple program

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,10 @@
+// This program outputs the message "Hello World!" to the monitor
+
+#include <iostream>                      // Gain access to the C++ standard library
+
+
+int main()                          // C++ programs start by executing the function main
+{
+    std::cout << "Hello, World!\n"; // Output "Hello, World!"
+    return 0;
+}
