ac7fac2d016064a4ca649c905ec244720f618bd5
Nickson Rop <nickyrutto@gmail.com>
2026-06-10 09:31:32 +0300

    feat: this program illustrates errors raised by the compiler when using illegal names in a program

---
+++ objects_types_variables/excercise2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+
+int main()
+{
+    int double = 0;
+    int x = 1;
+    int _h = 8;
+    int 2H = 5;
+    double h@x = 1;
+    int h.x = 33;
+    int h_x = 0;
+    int String = 0;
+}
