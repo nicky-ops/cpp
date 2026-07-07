e737a498ca6c2dd76bde8ab66c9038df1cc10476
Nickson Rop <nickyrutto@gmail.com>
2026-07-07 09:54:11 +0300

    chaining case labels in switch statements

---
+++ computation/statements/example4.cpp
@@ -0,0 +1,25 @@
+// chaining case labels - same action for a set of values in a switch
+#include <iostream>
+
+
+int main()
+{
+    std::cout<<"Please enter a digit\n";
+    char a = 0;
+    std::cin>>a;
+
+
+    switch(a){
+        case '0':case '2':case '4':case '6':case '8':
+            std::cout<<"is even\n";
+            break;
+
+        case '1':case '3':case '5':case '7': case '9':
+            std::cout<<"is odd\n";
+            break;
+
+        default:
+            std::cout<<"is not a digit\n";
+            break;
+    }
+}
