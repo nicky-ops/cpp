7a671ae18260db37edb1c99471f76bb0185bb94f
Nickson Rop <nickyrutto@gmail.com>
2026-07-11 19:13:57 +0300

    declaring a function before its definition

--- computation/statements/functions/example.cpp
+++ computation/statements/functions/example.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 
-int square_by_addition(int a)
-{
-    int result = 0;
-    for (int i = 0; i<a; ++i)
-        result += a;
-    return result;
-}
+
+int square_by_addition(int); // declaration of square function
+
 int main()
 {
     int input_value = 0;
     std::cout<<"Enter an integer value: \n";
     std::cin>>input_value;
     std::cout<<"The square of "<<input_value<<" is "<<square_by_addition(input_value)<<"\n";
+}
+
+int square_by_addition(int a)
+{
+    int result = 0;
+    for (int i = 0; i<a; ++i)
+        result += a;
+    return result;
 }
